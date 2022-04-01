1a021461a3e739c16b06404b845bb461cc7c4be4
tammyzhang-1 <tz332@cornell.edu>
2022-04-01 02:09:40 +0000

    hardcode catalog pages for admin and consumer

---
+++ pages/detail.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Playful Plants</title>
+  <link rel="stylesheet" type="text/css" href="public/styles/site.css"/>
+</head>
+
+<body>
+  <header>
+    <h1>Playful Plants</h1>
+  </header>
+
+  <main>
+
+  </main>
+
+</body>
+
+</html>
--- router.php
+++ router.php
@@ -25,6 +25,8 @@ if (preg_match('/^\/public\//', $request_uri) || $request_uri == '/favicon.ico')
   return False;
 } else if (match_routes($request_uri, '/')) {
   require 'pages/home.php';
+} else if (match_routes($request_uri, '/admin')) {
+  require 'pages/home-admin.php';
 } else {
   error_log("  404 Not Found: " . $request_uri);
   http_response_code(404);
