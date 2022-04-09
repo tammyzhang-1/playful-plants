3b6637593bd4cc8ba8aad4ed12805ae33d590284
tammyzhang-1 <tz332@cornell.edu>
2022-04-09 19:25:09 +0000

    implement consumer detail dynamic output

--- pages/detail.php
+++ pages/detail.php
@@ -2,6 +2,13 @@
   $db = init_sqlite_db('db/site.sqlite', 'db/init.sql');
 
   $id = $_GET["id"] ?? NULL;
+
+  $records = exec_sql_query($db, "SELECT * FROM entries WHERE (id=$id);") -> fetchAll();
+  $record = $records[0];
+
+  $tag_list = exec_sql_query($db, "SELECT tags.id, tags.name
+  FROM (entry_tags INNER JOIN tags ON entry_tags.tag_id = tags.id)
+  WHERE (entry_tags.entry_id = $id);") -> fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -26,25 +33,50 @@
   <main>
   <div class="detail-page">
     <div class="detail-photo">
-      <img src="public/images/FL_27.jpg" alt="">
+      <img src="public/images/<?php echo $record["plant_id"]; ?>.jpg" onerror=this.src="public/images/default.png" alt=""/>
     </div>
     <div class="detail-text">
       <div class="garden-list">
-        <h2>High Mallow</h2>
-        <h3>Gardening care:</h3>
+        <h2><?php echo ucwords($record["name"]); ?></h2>
+        <h3>Gardening information:</h3>
         <ul>
-          <li>Perennial</li>
+          <?php
+          foreach ($tag_list as $tag) { ?>
+            <?php echo "<li>" . $tag["name"] . "</li>"; ?>
+          <?php } ?>
+          <!-- <li>Perennial</li>
           <li>Full sun, partial shade</li>
           <li>Hardiness Zone Range: 4-8</li>
-          <li>Type: flower</li>
+          <li>Type: flower</li> -->
         </ul>
       </div>
       <div class="play-list">
         <h3>Types of play supported:</h3>
           <ul>
-            <li>Type 1</li>
-            <li>Type 2</li>
-            <li>Type 3</li>
+          <?php if ($record["exploratory_constructive_play"]) { ?>
+                <li>Exploratory Constructive Play</li>
+              <?php } ?>
+              <?php if ($record["exploratory_sensory_play"]) { ?>
+                <li>Exploratory Sensory Play</li>
+              <?php } ?>
+              <?php if ($record["physical_play"]) { ?>
+                <li>Physical Play</li>
+              <?php } ?>
+              <?php if ($record["imaginative_play"]) { ?>
+                <li>Imaginative Play</li>
+              <?php } ?>
+              <?php if ($record["restorative_play"]) { ?>
+                <li>Restorative Play</li>
+              <?php } ?>
+              <?php if ($record["expressive_play"]) { ?>
+                <li>Expressive Play</li>
+              <?php } ?>
+              <?php if ($record["play_with_rules"]) { ?>
+                <li>Play with Rules</li>
+              <?php } ?>
+              <?php if ($record["bio_play"]) { ?>
+                <li>Bio Play</li>
+              <?php } ?>
           </ul>
       </div>
     </div>
